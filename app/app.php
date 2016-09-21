231f129915e85aa7bfd2b0c12cf627cfdb8aac15
HK Kahng and matthew brandenburg <matthew@>
2016-09-21 13:51:25 -0700

    add front end to manage cuisine types

--- app/app.php
+++ app/app.php
@@ -22,7 +22,38 @@
         return $app['twig']->render('home.html.twig');
     });
 
-    
+    $app->get("/cuisines", function() use ($app) {
+        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/cuisines", function() use ($app) {
+        $cuisine_name = $_POST['cuisine_name'];
+        $new_cuisine = new Cuisine($cuisine_name);
+        $new_cuisine->save();
+        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->patch("/cuisines/{id}", function($id) use ($app) {
+        $new_cuisine_name = $_POST['new_cuisine_name'];
+        $cuisine = Cuisine::find($id);
+        $cuisine->update($new_cuisine_name);
+        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->delete("/cuisines/{id}", function($id) use ($app){
+        $cuisine = Cuisine::find($id);
+        $cuisine->delete();
+        return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->get("/cuisines/{id}", function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisines.html.twig', array('cuisine' => $cuisine));
+    });
+
+    $app->get("/restaurants", function() use ($app){
+        return $app['twig']->render('restaurants.html.twig');
+    });
 
     return $app;
 ?>
